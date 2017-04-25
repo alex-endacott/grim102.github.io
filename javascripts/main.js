889d787b8972b41c2fd6a0824e62579124535185
Grim102 <endacott.alex@gmail.com>
2017-04-25 18:06:58 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,26 @@
-console.log('This would be the main JS file.');
+var currentIndex = 0,
+  items = $('.container div'),
+  itemAmt = items.length;
+
+function cycleItems() {
+  var item = $('.container div').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+var autoSlide = setInterval(function() {
+  //currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 3000);
+
+$('.next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
