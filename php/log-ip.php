e87e79958183d19296f9532b296f4cde5ee2994f
Alex Endacott <endacott.alex@gmail.com>
2017-06-18 21:31:09 -0500

    Update log-ip.php

--- php/log-ip.php
+++ php/log-ip.php
@@ -1,1 +1,9 @@
-
+<?php
+$iplogfile = 'logs/ip-logs.txt';
+$ipaddress = $_SERVER['REMOTE_ADDR'];
+$timestamp = = date('d/m/Y h:i:s'):
+$fp = fopen($iplogfile, 'a+');
+chmod($iplogfile, 0777);
+fwrite($fp, '['.$timestamp.']: '.$ipaddress.' PHP_EOL);
+fclose($fp);
+?>
