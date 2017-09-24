76b72f70b2b5b0d52767a8f32c41d808ffe30a42
Grim102 <endacott.alex@gmail.com>
2017-09-24 11:14:06 -0500

    finish project

--- javascripts/wikipedia.js
+++ javascripts/wikipedia.js
@@ -1,14 +1,32 @@
 $( document ).ready(function() {
 
-	var wiki = $.ajax("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&origin=*&format=json");
+	$('#button').on('click', function() {
 
-	wiki.done(function(data, status, wiki) {
-		$('#posts').append('<section class="col-sm-12"><h1>' +
-							+ data.title + '</h1></section>')
+		$('#posts').html('');
+
+		var input = $(":input");
+
+		var title = input[0].value;
+
+		var wiki = $.ajax("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + title + "&limit=10&origin=*&format=json");
+
+		wiki.done(function(data, status, wiki) {
+			//console.log(data);
+			for(var i = 0; i < data[1].length; i++) {
+				$('#posts').append('<section class="col-sm-12"><h1>'
+							+ data[1][i] + '</h1>' 
+							+ '<p>' + data[2][i] + '</p>'
+							+ '<a href="' + data[3][i] + '" target="blank">Read more</a></section>')
+				console.log(data[3][i]);
+
+			}
+		});
+
+		wiki.fail(function(wiki, status, error) {
+			$('#posts').text("No results found");
+		});
+
+		return false;
+
 	});
-
-	wiki.fail(function(wiki, status, error) {
-		$('#posts').text("No results found");
-	});
-
 });
