33be5a1d8c39d21c7c61fd76ac7a8e006a7e92e7
Grim102 <endacott.alex@gmail.com>
2017-09-24 01:01:06 -0500

    add ajax request

--- javascripts/wikipedia.js
+++ javascripts/wikipedia.js
@@ -1,9 +1,14 @@
 $( document ).ready(function() {
 
-	var wiki = $.ajax("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json");
+	var wiki = $.ajax("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&origin=*&format=json");
 
 	wiki.done(function(data, status, wiki) {
-		console.log(wiki);
+		$('#posts').append('<section class="col-sm-12"><h1>' +
+							+ data.title + '</h1></section>')
+	});
+
+	wiki.fail(function(wiki, status, error) {
+		$('#posts').text("No results found");
 	});
 
 });
