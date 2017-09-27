257efaf1cdfcfc2e5caa7b7913eff6d915bda425
Grim102 <endacott.alex@gmail.com>
2017-09-27 13:31:35 -0500

    connect to api

--- javascripts/twitch.js
+++ javascripts/twitch.js
@@ -1,13 +1,16 @@
 $( document ).ready(function() {
 
-	var twitch = $.ajax('https://api.twitch.tv/helix/streams/metadata');
-
-	twitch.done(function(data, status, twitch) {
-		console.log(data);
-	});
-
-	twitch.fail(function(twitch, status, error) {
-
+	$.ajax({
+		type: 'GET',
+		url: 'https://api.twitch.tv/helix/streams?user_login=freecodecamp',
+		headers: {
+			'Client-ID': '7b4bf99huxakhzrve0wunghvaoh5h9'
+		},
+		success: function(data) {
+			console.log(data);
+		}
 	});
 
 });
+
+
