89e1805c34fa0a4d4f98ebf1a04b69aab79ec775
Grim102 <endacott.alex@gmail.com>
2017-12-03 21:58:06 -0600

    start clock and controls styling and jquery

--- javascripts/pomodoro.js
+++ javascripts/pomodoro.js
@@ -1,3 +1,6 @@
 $( document ).ready(function() {
-    
+    var min = $("#session-min").text();
+    var sec = $("#session-sec").text();
+    $("#clock-min").html(min);
+    $("#clock-sec").html(sec);
 });
