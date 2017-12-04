6b6e01e123ebf6841a4923e3250b57139a0bab31
Grim102 <endacott.alex@gmail.com>
2017-12-04 16:16:19 -0600

    start button logic

--- javascripts/pomodoro.js
+++ javascripts/pomodoro.js
@@ -1,6 +1,33 @@
 $( document ).ready(function() {
+
     var min = $("#session-min").text();
     var sec = $("#session-sec").text();
-    $("#clock-min").html(min);
-    $("#clock-sec").html(sec);
+
+    setClock();
+
+    function setClock() {
+    	$("#clock-min").html(min);
+    	$("#clock-sec").html(sec);
+    }
+
+    function setSession() {
+    	$("#session-min").html(min);
+    }
+
+    $("#session-plus").click(function(){
+    	if(min < 59) {
+			min++;
+			setSession();
+			setClock();
+		}
+	});
+
+	$("#session-minus").click(function(){
+		if(min > 0) {
+			min--;
+			setSession();
+			setClock();
+		}
+	});
+	
 });
